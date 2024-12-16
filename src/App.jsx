7ecc438b429a8402b11463a2326a1b90267908cb
Maarij07 <syedmuhammadmaarij@gmail.com>
2024-12-16 12:30:09 +0500

    brands added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Home from './components/Home';
 import Screen2 from './components/Screen2';
 import Screen3 from './components/Screen3';
 import Screen4 from './components/Screen4';
+import Screen5 from './components/Screen5';
+import Screen6 from './components/Screen6';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Screen2/>
       <Screen3/>
       <Screen4/>
+      <Screen5/>
+      <Screen6/>
     </div>
   );
 }
